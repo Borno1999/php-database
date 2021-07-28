23e2f4e0a160a2b270e7f3b4e5fe496ccd1d50fa
Borno1999 <84654560+Borno1999@users.noreply.github.com>
2021-07-28 08:32:23 +0600

    Update DBconnect.php

--- DBconnect.php
+++ DBconnect.php
@@ -1,7 +1,7 @@
 <?php
     function connect()
     {
-        $conn = new mysqli("localhost", "mmk", "123", "wtk");
+        $conn = new mysqli("localhost", "mmk", "123", "wtm");
         if($conn->connect_errno)
         {
             die("Database connection failed......" . $conn->connect_error);
